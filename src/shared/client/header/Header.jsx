9b1e10b4a4af00641a12d1ed4ea697e393e941fb
Santosh Dhakal <santosh.dhakal07@gmail.com>
2023-09-18 09:54:42 -0400

    removed comment, added mediaquery

--- src/shared/client/header/Header.jsx
+++ src/shared/client/header/Header.jsx
@@ -14,26 +14,6 @@ const Header = () => {
       </HeaderContainer>
     </MainContainer>
   );
-
-  // return (
-  //   <section className={styles.header}>
-  //     <section className={styles["header-top"]}>
-  //       <section className={styles["header-top__logo"]}>
-  //         <a href="/client" className={styles["header-logo"]}>
-  //           <i>
-  //             <strong>PUFI-2</strong>
-  //           </i>
-  //         </a>
-  //       </section>
-  //       <section className={styles["header-top__navbar"]}>
-  //         <section className={styles["header-top__navigation"]}>
-  //           <Navbar />
-  //         </section>
-  //         <hr className={styles["header-top__seperator"]} />
-  //       </section>
-  //     </section>
-  //   </section>
-  // );
 };
 
 export default Header;
--- src/shared/client/header/styled.js
+++ src/shared/client/header/styled.js
@@ -1,3 +1,4 @@
+import { breakpoint } from "@/styles/breakpoints";
 import styled from "@emotion/styled";
 import { Button } from "@mui/material";
 
@@ -15,12 +16,16 @@ export const HeaderContainer = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 0 24px;
-  margin-bottom: 20px;
+  padding: 0 16px;
 
   & > span {
     font-size: 40px;
     color: white;
     font-weight: 700;
+  }
+
+  @media only screen and (min-width: ${breakpoint.desktop}) {
+    padding: 0 24px;
+    margin-bottom: 20px;
   }
 `;
