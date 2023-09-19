594bdd39c8ca313b9cdbd4943c1f9abaf95da497
Santosh Dhakal <santosh.dhakal07@gmail.com>
2023-09-19 07:13:20 -0400

    Removed globalstyles

--- src/shared/layout/layout.jsx
+++ src/shared/layout/layout.jsx
@@ -1,7 +1,6 @@
 import { useRouter } from "next/router";
 import ClientLayout from "./ClientLayout";
 import ClinicianLayout from "./ClinicianLayout";
-import { inputGlobalStyles } from "./client-shared";
 
 const Layout = ({ children }) => {
   const router = useRouter();
@@ -9,10 +8,7 @@ const Layout = ({ children }) => {
   const isClient = router.pathname.startsWith("/client");
 
   return isClient ? (
-    <>
-      {inputGlobalStyles}
-      <ClientLayout>{children}</ClientLayout>
-    </>
+    <ClientLayout>{children}</ClientLayout>
   ) : (
     <ClinicianLayout>{children}</ClinicianLayout>
   );
