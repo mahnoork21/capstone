619a74f06af2a00d48339bbf030e20da8863849c
Alla-Anastasiia Gnatkiv <alla.gnatkiv@gmail.com>
2023-11-15 17:32:46 -0500

    update csv data to show values instead 0,1,2 to text description

--- src/scene/clinician/survey-report/helpers/download-helper.js
+++ src/scene/clinician/survey-report/helpers/download-helper.js
@@ -1,9 +1,19 @@
+import { youngChildSurvey } from "@/scene/client/survey/helper/youngChildSurvey";
+
 export const parseDataToCsvFormatYoungChild = (surveyData) => {
   const csvData = Object.entries(surveyData["activity_response"])
     .flatMap(([key, val]) =>
-      Object.entries(val).map(([questionId, v]) => ({
-        [key + "_" + questionId]: v.value,
-      }))
+      Object.entries(val).map(([questionId, v]) => {
+        const question = youngChildSurvey.find(
+          (q) => q.questionId === questionId
+        );
+        const labelShort =
+          question?.options.find((opt) => opt.value === v.value)?.labelShort ||
+          "";
+        return {
+          [key + "_" + questionId]: labelShort,
+        };
+      })
     )
     .reduce((acc, obj) => ({ ...acc, ...obj }), {});
 
@@ -16,7 +26,7 @@ export const parseDataToCsvFormatYoungChild = (surveyData) => {
 
   const csvHeader = Object.keys(sortedCsvData);
 
-  console.log("csvHeader ->", csvHeader);
+  console.log("sortedCsvData ->", sortedCsvData);
 
   return [csvHeader, sortedCsvData];
 };
