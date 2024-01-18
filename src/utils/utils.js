2d0cc9bab09c61776477acf107cd0191eaaca677
Ajmal Khan <akhan@hollandbloorview.ca>
2024-01-18 13:34:11 -0500

    Revert text change for one of the questions, as it was done incorrectly.

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -203,7 +203,7 @@ export const getLocaleYoungChildSurvey = (intl) => {
         },
         {
           labelShort: "Cannot do",
-          label: "Even with help, my child cannot do it using the prosthesis",
+          label: "Even with help, my child cannot do it without the prosthesis",
           miniGuideType: "difficultyScale-cannotDo",
           value: 0,
         },
